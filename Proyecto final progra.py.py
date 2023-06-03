2e878d05ea612a82f545e55ce909dd60fa3db4f2
Andres <fergonalcantara@gmail.com>
2023-06-03 11:37:33 -0600

    Reestructuracion del proyecto, pintado del tablero y calculos internos

--- Proyecto final progra.py.py
+++ Proyecto final progra.py.py
@@ -1,60 +1,136 @@
 import tkinter as tk
 import random
-#import numpy as np //aun no esta agregado, pero se utilizara
+import numpy as np
+
+def f_clic_boton(fila, columna):
+    # Obtenmos el valor del boton que fue pulsado
+    boton_clic = ventana_matriz.grid_slaves(row=fila, column=columna)[0]
+    valor_boton = boton_clic.cget("text")
+    boton_clic.configure(bg="yellow")
+    print("Valor del botón clickeado:", valor_boton)
+
+    # Obtenemos los valores de los botones que rodean al boton pulsado y los guardamos en un vector
+    valores_adyacentes = []
+
+    # Verificamos si hay botón encima del boton pulsado
+    if fila > 0:
+        bt_arriba = ventana_matriz.grid_slaves(row=fila-1, column=columna)[0]
+        valor_arriba = bt_arriba.cget("text")
+        bt_arriba.configure(bg="yellow")
+        valores_adyacentes.append(valor_arriba)
+
+    # Verificamos si hay botón abajo del boton pulsado
+    if fila < tamanio_matriz_ingresado - 1:
+        bt_abajo = ventana_matriz.grid_slaves(row=fila+1, column=columna)[0]
+        valor_abajo = bt_abajo.cget("text")
+        bt_abajo.configure(bg="yellow")
+        valores_adyacentes.append(valor_abajo)
+
+    # Verificamos si hay botón a la izquierda del boton pulsado
+    if columna > 0:
+        bt_izq = ventana_matriz.grid_slaves(row=fila, column=columna-1)[0]
+        valor_izq = bt_izq.cget("text")
+        bt_izq.configure(bg="yellow")
+        valores_adyacentes.append(valor_izq)
+
+    # Verificamos si hay botón a la derecha del boton pulsado
+    if columna < tamanio_matriz_ingresado - 1:
+        bt_der = ventana_matriz.grid_slaves(row=fila, column=columna+1)[0]
+        valor_derecha = bt_der.cget("text")
+        bt_der.configure(bg="yellow")
+        valores_adyacentes.append(valor_derecha)
+
+    # Verificamos esquina superior izquierda
+    if fila > 0 and columna > 0:
+        bt_izq_arr = ventana_matriz.grid_slaves(row=fila-1, column=columna-1)[0]
+        valor_izq_arr = bt_izq_arr.cget("text")
+        bt_izq_arr.configure(bg="yellow")
+        valores_adyacentes.append(valor_izq_arr)
+
+    # Verificamos esquina superior derecha
+    if fila > 0 and columna < tamanio_matriz_ingresado - 1:
+        bt_der_arr = ventana_matriz.grid_slaves(row=fila-1, column=columna+1)[0]
+        valor_der_arr = bt_der_arr.cget("text")
+        bt_der_arr.configure(bg="yellow")
+        valores_adyacentes.append(valor_der_arr)
+
+    # Verificamos esquina inferior izquierda
+    if fila < tamanio_matriz_ingresado - 1 and columna > 0:
+        boton_izq_abj = ventana_matriz.grid_slaves(row=fila+1, column=columna-1)[0]
+        valor_izq_abj = boton_izq_abj.cget("text")
+        boton_izq_abj.configure(bg="yellow")
+        valores_adyacentes.append(valor_izq_abj)
+
+    # Verificamos esquina inferior derecha
+    if fila < tamanio_matriz_ingresado - 1 and columna < tamanio_matriz_ingresado - 1:
+        bt_der_abj = ventana_matriz.grid_slaves(row=fila+1, column=columna+1)[0]
+        valor_der_abj = bt_der_abj.cget("text")
+        bt_der_abj.configure(bg="yellow")
+        valores_adyacentes.append(valor_der_abj)
+
+    print("Valores de los botones adyacentes:", valores_adyacentes)
+    np_valoresAdyacentes = np.array(valores_adyacentes)
+    resultado = np_valoresAdyacentes.sum() * valor_boton
+    print("El resultado de la operacion es: ", resultado)
 
 
-def boton_clicado():
-    print("Has clicado un botón")
+def crear_matriz(tamanio):
+    global ventana_matriz
+    ventana_matriz = tk.Toplevel(ventanaPrincipal)
 
-def generar_numero():
-    return random.randint(0, 11)
+    ventana_matriz.title("Matriz de Botones")
+    jugador1 = jugador1_entry.get()
+    jugador2 = jugador2_entry.get()
+    jugador1_label = tk.Label(ventanaPrincipal, text=jugador1)
+    jugador1_label.pack()
+    jugador2_label = tk.Label(ventanaPrincipal, text=jugador2)
+    jugador2_label.pack()
 
-def crear_matriz():
-    global jugador1
-    global jugador2
-    global n
-    n = int(entry3.get())
-    jugador1 = (entry1.get())
-    jugador2 = (entry2.get())
+    global tamanio_matriz_ingresado
+    tamanio_matriz_ingresado = tamanio
 
-    for i in range(n):
-        for j in range(n):
-            numero = generar_numero()
-            boton = tk.Button(frame, text=str(numero), command=boton_clicado, width=2,height=2)
-            boton.grid(row=i, column=j)
-    label = tk.Label(ventanaPrincipal, text= jugador1)
-    label.pack()
-    label = tk.Label(ventanaPrincipal, text= "VS")
-    label.pack()
-    label = tk.Label(ventanaPrincipal, text= jugador2)
-    label.pack()
+    for i in range(tamanio):
+        for j in range(tamanio):
+            valor = random.randint(0,11)
+            button = tk.Button(ventana_matriz, text=valor, width=10, height=5, command=lambda fila=i, columna=j: f_clic_boton(fila, columna),bg="black")
+            button.grid(row=i, column=j)
+    
+
+def iniciarJuego():
+    tamanio_matriz = int(tamanio_entry.get())
+    crear_matriz(tamanio_matriz)
 
 ventanaPrincipal = tk.Tk()
-ventanaPrincipal.title("Matriz Aritmetica")
-#ventanaPrincipal.geometry("450x450")
-ventanaPrincipal.resizable(0,0)
+ventanaPrincipal.title("Ventana Principal")
+ventanaPrincipal.geometry("450x450")
 ventanaPrincipal.iconbitmap("Logo-Meso-Color.ico")
 
-frame = tk.Frame(ventanaPrincipal)
-frame.pack()
+# Etiqueta y entrada para el jugador 1
+jugador1_label = tk.Label(ventanaPrincipal, text="Jugador 1:")
+jugador1_label.pack()
+jugador1_entry = tk.Entry(ventanaPrincipal)
+jugador1_entry.pack()
 
-label = tk.Label(ventanaPrincipal, text="Ingrese el nombre del primer jugador:")
-label.pack()
-entry1 = tk.Entry(ventanaPrincipal)#lectura del nombre del primer jugador 
-entry1.pack()
-label = tk.Label(ventanaPrincipal, text="Ingrese el nombre del segundo jugador:")
-label.pack()
-entry2 = tk.Entry(ventanaPrincipal)#lectura del nombre del segundo jugador
-entry2.pack()
-label = tk.Label(ventanaPrincipal, text="Ingrese el tamaño de la matriz:")
-label.pack()
-entry3 = tk.Entry(ventanaPrincipal)#lectura del tamaño de la matriz
-entry3.pack()
+# Etiqueta y entrada para el jugador 2
+jugador2_label = tk.Label(ventanaPrincipal, text="Jugador 2:")
+jugador2_label.pack()
+jugador2_entry = tk.Entry(ventanaPrincipal)
+jugador2_entry.pack()
 
-label = tk.Label(ventanaPrincipal, text="Ingrese el tamaño de la matriz:")
-label.pack()
+# Etiqueta y entrada para el tamaño de la matriz
+tamanio_label = tk.Label(ventanaPrincipal, text="Tamaño de la matriz:")
+tamanio_label.pack()
+tamanio_entry = tk.Entry(ventanaPrincipal)
+tamanio_entry.pack()
 
-boton = tk.Button(ventanaPrincipal, text="Crear matriz", command=crear_matriz)
-boton.pack()
+# Cantidad de turnos por jugador
+turnos_label = tk.Label(ventanaPrincipal, text="Ingrese la cantidad de Turnos por Jugador:")
+turnos_label.pack()
+turnos_label = tk.Entry(ventanaPrincipal)
+turnos_label.pack()
+
+# Botón para iniciar el juego
+boton_inicio = tk.Button(ventanaPrincipal, text="Iniciar Juego", command=iniciarJuego)
+boton_inicio.pack()
 
 ventanaPrincipal.mainloop()
